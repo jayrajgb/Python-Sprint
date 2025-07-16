f523f798009499de63cb977a913b4b06ec23ec04
jayrajgb <jayrajgborate11@gmail.com>
2025-07-16 20:49:49 +0530

    sets

--- tuples.py
+++ tuples.py
@@ -55,7 +55,7 @@
 ## Deletion
 # thistuple = ("apple", "banana", "cherry")
 # del thistuple
-# print(thistuple) #this will raise an error because the tuple no longer exists
+# print(thistuple) # this will raise an error because the tuple no longer exists
 
 
 ### Unpacking Tuples
